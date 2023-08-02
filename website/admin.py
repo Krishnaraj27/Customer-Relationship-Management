a075f442fa59c39e1eed8302868587458b5ef1de
Krishnaraj27 <gkrishnaraj2002@gmail.com>
2023-08-02 20:14:08 +0530

    added models Item and Category

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Record
+from .models import Record, Category, Item
 
 admin.site.register(Record)
+admin.site.register(Category)
+admin.site.register(Item)
--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,6 +15,29 @@ class Record(models.Model):
     def __str__(self):
         return (f"{self.first_name} {self.last_name}")
         
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, related_name="items", on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 
